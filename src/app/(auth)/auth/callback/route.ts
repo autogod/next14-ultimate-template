e1991223346ff00878889cab7728eadf92058e83
ysung327 <ysung327@naver.com>
2024-08-09 16:56:25 +0900

    add profile logic with comments and next redirection

--- src/app/(auth)/auth/callback/route.ts
+++ src/app/(auth)/auth/callback/route.ts
@@ -1,19 +1,40 @@
-// Supabase Auth Callback Route for OAuth2 PKCE Flow
+// oauth login callback route
 import { NextResponse } from "next/server";
-import { createClient } from "@/utils/supabase/server"; // The client you created from the Server-Side Auth instructions
+// The client you created from the Server-Side Auth instructions
+import { createClient } from "@/utils/supabase/server";
+import { prisma } from "@/utils/prisma";
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
-
   // if "next" is in param, use it as the redirect URL
-  const next = searchParams.get("next") ?? "/";
-
+  // "next"가 매개변수에 있으면 리디렉션 URL로 사용합니다.
+  let next = searchParams.get("next") ?? "/";
   if (code) {
     const supabase = createClient();
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    const { data, error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
+      // if the user doesn't have profile data yet, send them to the onboarding page
+      // const profile = await prisma.$transaction(async (prisma) => {
+      //   return await prisma.profile.findFirst({
+      //     where: {
+      //       user: data.user.id,
+      //     },
+      //   });
+      // });
+      // if (!profile) {
+      //   next = "/onboarding";
+      //   // create a profile for the user
+      //   await prisma.profile.create({
+      //     data: {
+      //       user: data.user.id,
+      //       profile_image_url: data.user.user_metadata.avatar_url,
+      //       name: data.user.user_metadata.full_name,
+      //     },
+      //   });
+      // }
+
+      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer // 로드 밸런서 이전의 원래 원본
       const isLocalEnv = process.env.NODE_ENV === "development";
       if (isLocalEnv) {
         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
@@ -27,9 +48,10 @@ export async function GET(request: Request) {
   }
 
   // return the user to an error page with instructions
+  // 사용자를 오류 페이지로 리디렉션하고 지침을 제공합니다.
   return NextResponse.redirect(
     `${origin}/login?message=${encodeURIComponent(
-      "There was an error logging in. Please try again."
+      "Login failed. Please try again."
     )}`
   );
 }
--- src/app/(auth)/auth/confirm/route.ts
+++ src/app/(auth)/auth/confirm/route.ts
@@ -1,12 +1,13 @@
-// Supabase Auth Email OTP Confirmation Route
+// signup, email magic link confirmatiom route
 import { type EmailOtpType } from "@supabase/supabase-js";
 import { type NextRequest, NextResponse } from "next/server";
 
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
+import { prisma } from "@/utils/prisma";
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
+  const { searchParams, origin } = new URL(request.url);
   const token_hash = searchParams.get("token_hash");
   const type = searchParams.get("type") as EmailOtpType | null;
   const next = searchParams.get("next") ?? "/";
@@ -14,20 +15,44 @@ export async function GET(request: NextRequest) {
   if (token_hash && type) {
     const supabase = createClient();
 
-    const { error } = await supabase.auth.verifyOtp({
+    const { data, error } = await supabase.auth.verifyOtp({
       type,
       token_hash,
     });
-    if (!error) {
-      // redirect user to specified redirect URL or root of app
-      redirect(next);
+    if (!error && data?.user) {
+      // if the user doesn't have profile data yet, send them to the onboarding page
+      // const profile = await prisma.$transaction(async (prisma) => {
+      //   return await prisma.profile.findFirst({
+      //     where: {
+      //       user: data.user?.id,
+      //     },
+      //   });
+      // });
+      // if (!profile) {
+      //   // create a profile for the user
+      //   await prisma.profile.create({
+      //     data: {
+      //       user: data.user.id,
+      //     },
+      //   });
+      // }
+      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer // 로드 밸런서 이전의 원래 원본
+      const isLocalEnv = process.env.NODE_ENV === "development";
+      if (isLocalEnv) {
+        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+        return NextResponse.redirect(`${origin}${next}`);
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+      } else {
+        return NextResponse.redirect(`${origin}${next}`);
+      }
     }
   }
 
   // redirect the user to an error page with some instructions
   redirect(
     `/login?message=${encodeURIComponent(
-      "이메일 인증에 실패했습니다. 다시 시도해주세요."
+      "Email verification failed. Please try again."
     )}`
   );
 }
