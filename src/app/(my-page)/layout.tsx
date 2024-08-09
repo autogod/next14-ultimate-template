f76fa12415d151de80dd7268344c8e26e984e668
ysung327 <ysung327@naver.com>
2024-08-10 00:22:38 +0900

    add side nav

--- src/app/(my-page)/layout.tsx
+++ src/app/(my-page)/layout.tsx
@@ -1,10 +1,4 @@
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuList,
-} from "@/components/ui/navigation-menu";
-import { User, ReceiptText, TicketCheck, FileText } from "lucide-react";
-import Link from "@/components/layout/link";
+import { Sidebar } from "./sidebar";
 
 export default function MyPageLayout({
   children,
@@ -13,63 +7,8 @@ export default function MyPageLayout({
 }>) {
   return (
     <main className="flex w-full flex-1">
-      <aside className="flex flex-col justify-between max-w-48 bg-white shadow-s p-5">
-        <NavigationMenu
-          orientation="vertical"
-          className="items-start flex-none"
-        >
-          <NavigationMenuList className="flex-col space-x-0 items-start gap-2">
-            <NavigationMenuItem>
-              <Link href="/profile">
-                <div className="flex items-center">
-                  <User className="mr-3" size={16} strokeWidth={2} />
-                  프로필
-                </div>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/referral">
-                <div className="flex items-center">
-                  <TicketCheck className="mr-3" size={16} strokeWidth={2} />
-                  레퍼럴 관리
-                </div>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/subscription">
-                <div className="flex items-center">
-                  <ReceiptText className="mr-3" size={16} strokeWidth={2} />
-                  구독 관리
-                </div>
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-        <NavigationMenu
-          orientation="vertical"
-          className="items-start flex-none"
-        >
-          <NavigationMenuList className="flex-col space-x-0 items-start gap-2">
-            <NavigationMenuItem>
-              <Link href="/privacy">
-                <div className="flex items-center">
-                  <FileText className="mr-3" size={16} strokeWidth={2} />
-                  개인정보처리방침
-                </div>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/terms">
-                <div className="flex items-center">
-                  <FileText className="mr-3" size={16} strokeWidth={2} />
-                  이용약관
-                </div>
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-      </aside>
-      {children}
+      <Sidebar />
+      <div className="flex-1 px-6 py-4">{children}</div>
     </main>
   );
 }
